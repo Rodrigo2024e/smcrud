3e6a101da964e91087854ffb010e699d60177f2e
Rodrigo <lucianorodrigocarvalho@gmail.com>
2024-12-07 07:27:12 -0300

    GET Products paged

--- src/main/java/com/smartprocess/smcrud/controllers/ClientController.java
+++ src/main/java/com/smartprocess/smcrud/controllers/ClientController.java
@@ -1,8 +1,11 @@
 package com.smartprocess.smcrud.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +28,9 @@ public class ClientController {
 		return service.findById(id);
 	}
 		
+	@GetMapping
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
+	}
 
 }
--- src/main/java/com/smartprocess/smcrud/services/ClientService.java
+++ src/main/java/com/smartprocess/smcrud/services/ClientService.java
@@ -1,6 +1,10 @@
 package com.smartprocess.smcrud.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,4 +23,14 @@ public class ClientService {
 		Client client = repository.findById(id).get();
 		return new ClientDTO(client);
 	}
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new ClientDTO(x));
+	}
+	
+	
+	
+	
 }
